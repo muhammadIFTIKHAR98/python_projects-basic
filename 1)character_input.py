7f64c01f23a06ecd24f05e451470ba122aaf7810
Miftikhar98 <118063653+muhammadIFTIKHAR98@users.noreply.github.com>
2024-12-14 00:49:27 +0530

    Add files via upload

---
+++ 1)character_input.py
@@ -0,0 +1,11 @@
+#Firstly we need to specify the input required.
+# then we need to import datetime as we required the current year.
+#we will add the remaining age to reach 100 year age into the current year which we have imported from datetime. 
+
+import datetime
+name = str(input("what is your name: "))
+age = int(input("what is your age: "))
+
+year = (datetime.date.today().year) + (100-age)
+
+print(f"Hi, {name} you will be 100 years old in year{year}")
