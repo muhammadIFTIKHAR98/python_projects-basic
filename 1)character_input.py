7e2f968aa7b2eed1f2cb7b89f91ea24b578d37b8
Miftikhar98 <118063653+muhammadIFTIKHAR98@users.noreply.github.com>
2024-12-14 00:51:10 +0530

    Update 1)character_input.py

--- 1)character_input.py
+++ 1)character_input.py
@@ -2,10 +2,11 @@
 # then we need to import datetime as we required the current year.
 #we will add the remaining age to reach 100 year age into the current year which we have imported from datetime. 
 
+
 import datetime
 name = str(input("what is your name: "))
 age = int(input("what is your age: "))
 
 year = (datetime.date.today().year) + (100-age)
-
+#to mention any reference we need to provide f-string function inside the print function, else it will show an error.
 print(f"Hi, {name} you will be 100 years old in year{year}")
