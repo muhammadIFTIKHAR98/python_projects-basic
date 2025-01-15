0de4c55375b597893b3aefb4dcde0deba8a7a492
Miftikhar98 <118063653+muhammadIFTIKHAR98@users.noreply.github.com>
2025-01-15 23:46:58 +0530

    Update and rename BMI_CALCULATOR_ to BMI_CALCULATOR.py

--- BMI_CALCULATOR.py
+++ BMI_CALCULATOR.py
@@ -1,9 +1,16 @@
 #THIS THE CODE FOR CALCULATING THE BODY MASS INDEX
+
+#asking for the input
 Height=float(input("Enter your height in centimeters: "))
 Weight=float(input("Enter your Weight in Kg: "))
+
+#formula to calculate BMI
 Height = Height/100
 BMI=Weight/(Height*Height)
+
 print("your Body Mass Index is: ",BMI)
+
+# conditions
 if(BMI>0):
 	if(BMI<=16):
 		print("you are severely underweight")
